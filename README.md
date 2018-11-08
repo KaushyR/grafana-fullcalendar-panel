99eaffd91fb7c90877907f34b18179a52d3ac012
kaushyr <kaushy@gmail.com>
2018-11-09 03:51:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# grafana-fullcalendar-panel
---
+++ dist/module.js
@@ -0,0 +1,16 @@
+"use strict";
+
+System.register(["./fullcalendar_ctrl"], function (_export, _context) {
+  "use strict";
+
+  var FullCalendarCtrl;
+  return {
+    setters: [function (_fullcalendar_ctrl) {
+      FullCalendarCtrl = _fullcalendar_ctrl.default;
+    }],
+    execute: function () {
+      _export("PanelCtrl", FullCalendarCtrl);
+    }
+  };
+});
+//# sourceMappingURL=module.js.map
